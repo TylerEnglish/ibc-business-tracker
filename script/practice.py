e7012b4ada7e8803539dcff6141c55c91b5e107d
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2024-06-08 16:53:55 -0500

    Create practice.py

---
+++ script/practice.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read Excel file with multiple sheets
+xls = pd.read_excel("Student_Business_Template_Test.xlsx", sheet_name=['Daily Business Hours', 'Inventory'])
+
+# Access individual sheets using sheet names
+sheet1_df = xls['Daily Business Hours']
+sheet2_df = xls['Inventory']
+
+sheet1_df
